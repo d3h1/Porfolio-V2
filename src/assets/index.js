8468ca75fc48587ab82187d67b380bd67c788af8
d3h1 <denicabaravdic@gmail.com>
2023-04-18 12:18:11 -0700

    Starting a version 2 of my portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,87 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import cpp from "./tech/cpp.png";
+import csharp from "./tech/csharp.png";
+import sass from "./tech/sass.png";
+import mysql from "./tech/mysql.png";
+import npm from "./tech/npm.png";
+import aws from "./tech/aws.png";
+import jenkins from "./tech/jenkins.png";
+
+import disney from "./company/disney.png";
+import freelance from "./company/freelance.png";
+import startup from "./company/startup.png";
+
+
+import yelpclone from "./yelpclone.png";
+import portfolio from "./portfolio.png";
+import sudoku from "./sudoku.png";
+import stocks from "./stocks.png";
+import ai from "./ai.png";
+import money from "./money.png";
+import tmanage from "./tmanage.png";
+import githubprof from "./githubprof.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  python,
+  cpp,
+  csharp,
+  jenkins,
+  aws,
+  npm,
+  sass,
+  mysql,
+  disney,
+  freelance,
+  startup,
+  yelpclone,
+  portfolio,
+  sudoku,
+  stocks,
+  ai,
+  tmanage,
+  money,
+  githubprof,
+};
